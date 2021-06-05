3b921a7b2a3a179f6bf93704a45b9cbb771500f4
juliocesar <juliocesaar.ma@gmail.com>
2021-06-05 16:53:55 -0300

    Home screen home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,51 @@
 <template>
-  <hello-world />
+  <v-container class="home">
+    <v-card class="justify-center transparent" flat>
+      <v-row align="center" class="justify-center">
+        <v-col cols="4">
+          <v-img
+            :src="require('../assets/logo.png')"
+            class="my-5"
+            contain
+            height="90"
+          />
+          <v-text-field
+              prepend-inner-icon="mdi-magnify"
+              label="Copacabana"
+              class="input"
+              solo
+          ></v-text-field>
+          <v-row class="justify-center">
+            <v-col cols="6">
+              <v-btn
+                  tile
+                  color="success"
+                  class="py-4 px-10 mx-auto"
+              >
+                Pesquisar
+              </v-btn>
+            </v-col>
+          </v-row>
+
+        </v-col>
+      </v-row>
+    </v-card>  
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    name: 'Home'
   }
 </script>
+
+<style lang="sass">
+  .home
+    min-height: 100vh
+    display: grid
+    align-items: center
+    .input div
+      padding: 11px
+      border-radius: 8px
+</style>
