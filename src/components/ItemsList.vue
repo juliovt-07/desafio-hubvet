fc5667ff3b15ac22c8c337dee4b55d9a01c5f4ee
juliocesar <juliocesaar.ma@gmail.com>
2021-06-09 23:31:58 -0300

    Mock listing data

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -13,7 +13,7 @@
         <v-row>
           <v-col>
             <svg class="mx-1" v-for="(label, id) in item.labels" :key="label" width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="3.5" cy="3.5" r="3.5" :fill="getColor(item.labels[id])"/>
+              <circle cx="3.5" cy="3.5" r="3.5" :fill="colorLabels(item.labels[id])"/>
             </svg>
           </v-col>
         </v-row>
@@ -38,68 +38,7 @@
         { text: 'Preço (R$)', value: 'price' },
         { text: 'Etiquetas', value: 'labels' }
       ],
-      packages: [
-        {
-          code: 'HP556',
-          name: 'Final de ano em Copacabana',
-          price: '7.591,70',
-          labels: ['promocao', 'novos', 'ultimosdias']
-        },
-        {
-          code: 'HP504',
-          name: 'Carnaval em Copacabana',
-          price: '6.266,10',
-          labels: ['promocao', 'novos', 'ultimosdias']
-        },
-        {
-          code: 'HP556',
-          name: 'Feriadão em Copacabana',
-          price: '2.190,75',
-          labels: ['promocao', 'novos', 'ultimosdias']
-        },
-        {
-          code: 'HP589',
-          name: 'Semana Santa em Copacabana',
-          price: '3.490,11',
-          labels: ['promocao', 'novos', 'ultimosdias']
-        },
-        {
-          code: 'HP511',
-          name: 'Final de ano em Copacabana',
-          price: '7.591,70',
-          labels: ['promocao', 'novos']
-        },
-        {
-          code: 'HP556',
-          name: 'Carnaval em Copacabana',
-          price: '6.266,10',
-          labels: ['promocao', 'ultimosdias']
-        },
-        {
-          code: 'HP511',
-          name: 'Feriadão em Copacabana',
-          price: '2.190,75',
-          labels: ['novos']
-        },
-        {
-          code: 'HP556',
-          name: 'Semana Santa em Copacabana',
-          price: '3.490,11',
-          labels: ['novos', 'ultimosdias']
-        },
-        {
-          code: 'HP504',
-          name: 'Final de ano em Copacabana',
-          price: '7.591,70',
-          labels: ['promocao', 'novos', 'ultimosdias']
-        },
-        {
-          code: 'HP556',
-          name: 'Carnaval em Copacabana',
-          price: '6.266,10',
-          labels: ['ultimosdias']
-        },
-      ]
+      packages: require('../data/packages.json')
     }),
     methods: {
       colorLabels (label) {
