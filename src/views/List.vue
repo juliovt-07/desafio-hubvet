7fc8146f6adc918d4e0f221d0cb00abbbf5affb5
juliocesar <juliocesaar.ma@gmail.com>
2021-06-11 17:32:28 -0300

    Add elevation to list cards

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,11 +2,11 @@
   <v-container class="items-list">
     <TopBar/>
     <v-row align="top">
-      <v-col cols="10">
+      <v-col cols="9">
         <ItemsList/>
       </v-col>
-      <v-col cols="2">
-        <v-card min-width="300" min-height="300"></v-card>
+      <v-col cols="3">
+        <v-card elevation="4" class="mt-3" min-width="370" min-height="300"></v-card>
       </v-col>
     </v-row>
   </v-container>
