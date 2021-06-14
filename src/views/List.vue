7a8b5dc15fff632103fb114a7cbdd160c3b9a47a
juliocesar <juliocesaar.ma@gmail.com>
2021-06-14 04:34:49 -0300

    Add filters to the listing screen

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,24 @@
 <template>
   <v-container class="items-list">
     <TopBar/>
+    <v-row align="center" justify="space-between" class="px-2">
+      <v-col cols="4" class="mt-1">
+        <v-text-field
+            prepend-inner-icon="mdi-magnify"
+            label="Copacabana"
+            solo
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <InputText/>
+      </v-col>
+      <v-col cols="1" class="ml-n9 mr-md-6 mr-lg-0 mr-sm-3">
+        <CustomRadio/>
+      </v-col>
+      <v-col cols="5">
+        <SelectChips/>
+      </v-col>
+    </v-row>
     <v-row align="top">
       <v-col cols="9">
         <ItemsList/>
@@ -15,12 +33,18 @@
 <script>
 import TopBar from "@/components/TopBar";
 import ItemsList from "@/components/ItemsList";
+import InputText from "@/components/InputText";
+import CustomRadio from "@/components/CustomRadio";
+import SelectChips from "@/components/SelectChips";
 
   export default {
     name: 'List',
     components: {
       TopBar,
-      ItemsList
+      ItemsList,
+      InputText,
+      CustomRadio,
+      SelectChips
     }
   }
 </script>
